53fd28e293a878f023e7c4e1d2f649075c213b27
Mykyta Milshyn <m.milshyn.freelancer@gmail.com>
2025-04-22 01:48:31 +0300

    feature/create-products-module

--- src/product/product.router.js
+++ src/product/product.router.js
@@ -1,18 +1,60 @@
-const express = require('express');
-const db = require('../db');
-const { products } = require('../db/schema');
+import express from 'express'
+import { eq } from 'drizzle-orm'
+import { db } from '../db/index.js'
+import { products } from '../db/schema.js'
 
-const router = express.Router();
+const router = express.Router()
 
-router.post('/products', async (request, response) => {
-    const { body } = request;
-    await db.insert(products).values(body);
-    return response.sendStatus(201);
-});
+router.post('/products', async (req, res, next) => {
+    try {
+        await db.insert(products).values(req.body)
+        res.sendStatus(201)
+    } catch (err) {
+        next(err)
+    }
+})
 
-router.get('/products', async (request, response) => {
-    const products = await db.query.products.findMany();
-    return response.json(products);
-});
+router.delete('/products/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const found = await db
+            .select()
+            .from(products)
+            .where(eq(products.id, Number(id)))
+        if (found.length === 0) {
+            return res.status(404).json({ message: `Product with id = ${id} not found` })
+        }
+        await db
+            .delete(products)
+            .where(eq(products.id, Number(id)))
+        res.status(200).json({ message: `Product with id = ${id} deleted successfully` })
+    } catch (err) {
+        next(err)
+    }
+})
 
-module.exports = router;
+router.get('/products', async (req, res, next) => {
+    try {
+        const { name, brand, id } = req.query
+        let query = db.select().from(products)
+        if (name) {
+            query = query.where(eq(products.name, name))
+        }
+        if (brand) {
+            query = query.where(eq(products.brand, brand))
+        }
+        if (id) {
+            query = query.where(eq(products.id, Number(id)))
+        }
+        const list = await query
+
+        if (list.length === 0) {
+            return res.status(404).json({ message: 'No user records found in the database' })
+        }
+        res.json(list)
+    } catch (err) {
+        next(err)
+    }
+})
+
+export default router
